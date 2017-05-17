5ed4c392f0fa109e36fd919c902a206b9aeb7146
Mateusz Malenta <mateusz.malenta@gmail.com>
2017-05-17 17:17:27 +0100

    Ready for first tests

--- dedisperse.cu
+++ dedisperse.cu
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -6,6 +7,7 @@
 #include "filterbank.hpp"
 #include "kernels.cuh"
 
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
@@ -43,16 +45,67 @@ int main(int argc, char *argv[]) {
     // TODO: Set outbands to the nearest power of 2 for my own sake
 
     // NOTE: Assume the whole file will fit in the device memory at once for now
-    size_t insize = filhead.nsamps * filhead.nchans;      // total number of points in the input filterbank file
+    size_t insamps = filhead.nsamps;
+
+    size_t insize = insamps * filhead.nchans;      // total number of points in the input filterbank file
     unsigned char *indata;
     cudaCheckError(cudaMalloc((void**)&indata, insize * sizeof(unsigned char)));
 
-    unsigned int *delays;
-    cudaCheckError(cudaMalloc((void**)&delays, filhead.nchans * sizeof(unsigned int)));
+    unsigned int *ddelays;
+    cudaCheckError(cudaMalloc((void**)&ddelays, filhead.nchans * sizeof(unsigned int)));
     dim3 nthreads(min(filhead.nchans, 1024), 1, 1);
     dim3 nblocks(nthreads.x / filhead.nchans, 1, 1);
-    InitDelaysKernel<<<nblocks, nthreads, 0>>>(delays, filhead.nchans, dm, filhead.topfreq, filhead.chanband, filhead.tsamp);
+    InitDelaysKernel<<<nblocks, nthreads, 0>>>(ddelays, filhead.nchans, dm, filhead.topfreq, filhead.chanband, filhead.tsamp);
+    cudaCheckError(cudaDeviceSynchronize());
 
+    unsigned int *hdelays = new unsigned int[filhead.nchans];
+    cudaCheckError(cudaMemcpy(hdelays, ddelays, filhead.nchans * sizeof(unsigned int), cudaMemcpyDeviceToHost));
+
+    unsigned char *transdata;
+    cudaCheckError(cudaMalloc((void**)&transdata, filhead.nchans * filhead.nsamps * sizeof(unsigned char)));
+    nthreads.x = min(filhead.nchans, 1024);
+    nblocks.x = insamps;
+    unsigned int perthread = filchans.nchans / nthreads.x;
+    TransposeKernel<<<nblocks, nthreads, 0>>>(indata, transdata, perthread)
+    cudaCheckError(cudaDeviceSynchronize());
+
+    unsigned int maxdelay = hdelays[filhead.nchans - 1];
+    size_t outsamps = filhead.nsamps - maxdelay
+    size_t outsize = filhead.nchans * outsamps;
+    float *outdata;
+    cudaCheckError(cudaMalloc((void**)&outdata, outsize * sizeof(float)));
+
+    perblock = 4;
+    nblocks.x = (outsamps - 1 / perblock) + 1;
+    DedisperseKernel<<<nblocks, nthreads, 0>>>(transdata, outdata, ddelays, filhead.nsamps, outsamps, perblock, perthread);
+    cudaCheckError(cudaDeviceSynchronize());
+
+    float *saved = new float[outsize];
+    cudaCheckError(cudaMemcpy(saved, outdata, outsize * sizeof(float)));
+
+    std::ofstream outfile("dedisp.ascii");
+
+    if !(outfile) {
+        cerr << "Could not create the output file!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int ichan = 0; ichan < outbands; ichan++ {
+        for (unsigned int isamp = 0; isamp < outsamps; isamp++) {
+            outfile << saved[ichan * outsamps + isamp] << " ";
+        }
+        outfile << endl;
+    }
+
+    outfile.close();
+
+    delete [] saved;
+    delete [] hdelays;
+
+    cudaCheckError(cudaFree(outdata));
+    cudaCheckError(cudaFree(transdata));
+    cudaCheckError(cudaFree(ddelays));
+    cudaCheckError(cudaFree(indata));
 
     return 0;
 }
--- kernels.cu
+++ kernels.cu
@@ -9,3 +9,29 @@ __global__ void InitDelaysKernel(unsigned int *delays, unsigned int nchans, floa
     // NOTE: The below calculation is done for delay in milliseconds with frequencies in MHz
     delays[idx] = (int)(4.15e+06 * ( 1.0f / (fbott * fbott) - 1.0f / (ftop * ftop)) * dm / tsamp);
 }
+
+__global__ void TransposeKernel(unsigned char *indata, unsigned char *outdata, unsigned int insize, unsigned int perthread) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < insize) {
+        for (int ichan = 0; ichan < perthread; ichan++) {
+            outdata[(threadIdx.x * perthread + ichan) * gridDim.x + blockIdx.x] = indata[idx * perthread + ichan];
+        }
+    }
+}
+
+__global__ void DedisperseKernel(unsigned char *indata, float *outdata, unsigned int *delays, unsigned int insamples, unsigned int outsamples, unsigned int perblock, unsigned int perthread) {
+
+    unsigned int chanid;
+    unsigned int sampid;
+
+    for (unsigned int isamp = 0; isamp < perblock; isamp++) {
+        sampid = blockIdx.x * perblock + isamp;
+        if (sampid < outsamples) {
+            for (unsigned int ichan = 0; ichan < perthread; ichan ++) {
+                chanid = threadIdx.x * perthread + ichan;
+                outdata[chanid * outsamples + sampid] = (float)indata[chanid * insamples + sampid + delays[chanid]];
+            }
+        }
+    }
+
+}
